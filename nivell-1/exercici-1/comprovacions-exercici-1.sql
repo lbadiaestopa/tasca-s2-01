f25da069af044de472a9ebbd29437919b268c9f7
Lluc Badia Estopà <lbadiaestopa@gmail.com>
2026-03-08 12:17:44 +0100

    feat: add required tests

---
+++ nivell-1/exercici-1/comprovacions-exercici-1.sql
@@ -0,0 +1,19 @@
+SELECT *
+FROM sale s
+JOIN client c
+ON s.client_id = c.client_id
+WHERE s.client_id = 1;
+
+SELECT *
+FROM sale s
+JOIN employee c
+ON s.employee_id = c.employee_id
+WHERE s.employee_id = 1 AND s.sale_date BETWEEN '2026-01-01' AND '2026-12-31';
+
+SELECT DISTINCT
+    su.supplier_id,
+    su.name
+FROM sale s
+JOIN model m ON s.model_id = m.model_id
+JOIN brand b ON m.brand_id = b.brand_id
+JOIN supplier su ON b.supplier_id = su.supplier_id
